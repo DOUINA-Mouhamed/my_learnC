b2d36720527cd5466d2f43f78d1d7b62430e29fd
DOUINA <mouhamed.douina@epitech.eu>
2024-01-30 23:15:18 +0100

    All folders pushed

--- statements/1_if.c
+++ statements/1_if.c
@@ -3,28 +3,4 @@
 
 int main (void)
 {
-    int ch;
-    int age = 0;
-
-    initscr();
-    cbreak();
-    noecho();
-
-    printw("Enter your age : ");
-
-    while (1) {
-        ch = getch();
-
-        if (ch == 10) {
-            break;
-        }
-        if (isdigit(ch)) {
-            printw("%c", ch);
-            refresh();
-            age = age * 10 + (ch - '0');
-        }
-    }
-    endwin();
-    printf("Your entered age is %d\n", age);
-    return 0;
 }
