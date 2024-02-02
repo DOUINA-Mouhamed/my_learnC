a8b0eec101028f4f507f39404e7bf1ef6ad012b8
DOUINA <mouhamed.douina@epitech.eu>
2024-02-02 01:09:26 +0100

    All folders pushed

---
+++ my_learnC/02_statements/4_switch.c
@@ -0,0 +1,11 @@
+/*
+
+Les switchs sont une alternatives beaucoup plus efficaces aux "}else if", ils sont plus
+facile à lire et s'exécutent plus rapidement que les "else if".
+
+Compilation :
+
+gcc -o switch 4_switch.c
+
+*/
+
