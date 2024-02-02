693e68cced98b7bf4d9f40fee8b7612391815d99
DOUINA <mouhamed.douina@epitech.eu>
2024-02-02 01:10:29 +0100

    All folders pushed

--- my_learnC/02_statements/4_switch.c
+++ my_learnC/02_statements/4_switch.c
@@ -2,10 +2,10 @@
 
 Les switchs sont une alternatives beaucoup plus efficaces aux "}else if", ils sont plus
 facile à lire et s'exécutent plus rapidement que les "else if".
+Les switchs permettent de tester une valeur pour *égalité* contre plusieurs cas.
 
 Compilation :
 
 gcc -o switch 4_switch.c
 
 */
-
