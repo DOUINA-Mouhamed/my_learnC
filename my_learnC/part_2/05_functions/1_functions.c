d10ceb0eb81c6d8dce67fe6ad67e8864d7f81ed2
DOUINA Mouhamed <146713539+DOUINA-Mouhamed@users.noreply.github.com>
2024-02-07 05:15:09 +0100

    push 1_functions.c

--- my_learnC/part_2/05_functions/1_functions.c
+++ my_learnC/part_2/05_functions/1_functions.c
@@ -1,1 +1,22 @@
-mm
+/*
+
+Une fonction est un petit segment de code qui est exécuté (appel de fonction)
+Quand on compile des fichiers C, on commence avec un appel de la fonction 'main'
+
+Compilation :
+
+gcc -o functions 1_functions.c
+
+*/
+
+void bonjour(void)
+{
+    printf("Bonjour !\n");
+    printf("Quelle belle journée !");
+}
+
+int main(void)
+{
+    bonjour();  // On appelle la fonction au lieu d'écrire dans le main
+    bonjour();  // On peut l'appeller une nouvelle fois au lieu d'écrire
+}               // plusieurs fois la même chose
