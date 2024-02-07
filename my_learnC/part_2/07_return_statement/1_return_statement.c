c069f4538a54ceeb52e02fd65bc746131246fd93
DOUINA Mouhamed <146713539+DOUINA-Mouhamed@users.noreply.github.com>
2024-02-07 05:43:47 +0100

    Update 1_return_statement.c

---
+++ my_learnC/part_2/07_return_statement/1_return_statement.c
@@ -0,0 +1,30 @@
+/*
+
+Nous allons parler des 'return statements'.
+Un return est une instruction utilisée pour
+renvoyer une valeur à partir d'une fonction à l'endroit où elle a été appelée.
+Lorsqu'une fonction atteint une instruction 'return', elle arrête don exécution
+et renvoie la valeur spécifiée dans l'instruction 'return'.
+Ce cours complète bien la partie '05_functions'.
+
+Compilation :
+
+gcc -o arguments arguments.c
+
+*/
+
+double square(double x)         // Le type de fonction change selon le ce que l'on veut return.
+{
+    //double result = x * x;
+    //return result;            // Ici on return result, qui est un double, donc la fonction est de type double.
+
+    return x * x;               // Manière plus optimisée
+}                               // Si l'on veut retourner un char, la fonction sera de type 'char', avec un int pareil, etc...
+
+int main(void)
+{
+    double x = square(10);
+    printf("%lf\n", x);
+
+    return 0;                   // Retour de '0' et main est de type int
+}
