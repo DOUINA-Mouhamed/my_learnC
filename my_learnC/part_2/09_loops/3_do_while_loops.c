cc7ca598ca087a0f3fe52b9f75921ba37f2abb45
DOUINA Mouhamed <146713539+DOUINA-Mouhamed@users.noreply.github.com>
2024-02-09 01:23:56 +0100

    Update 3_do_while_loops.c

--- my_learnC/part_2/09_loops/3_do_while_loops.c
+++ my_learnC/part_2/09_loops/3_do_while_loops.c
@@ -4,3 +4,21 @@ un 'do while loop' exécute d'abord un bloc de code,
 puis ensuite vérifie la condition.
 
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int number = 0;
+    int sum = 0;
+
+    do {        // Initialisation de do
+        printf("Enter a number above 0 or a negative one to exit : ");
+        scanf("%d", &number);
+        if (number > 0) {
+            sum += number;
+        }
+    } while(number > 0);    // Vérification de condition après exécution
+    printf("Sum : %d", sum);
+    return 0;
+}
